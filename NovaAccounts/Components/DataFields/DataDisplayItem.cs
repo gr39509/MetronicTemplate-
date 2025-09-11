a29e43e060fd69bc8067aecca83dbf62f54f841f
Tahiru22 <ktahiru181@gmail.com>
2025-09-11 17:42:00 +0000

    Fixed outdated dependencies

---
+++ NovaAccounts/Components/DataFields/DataDisplayItem.cs
@@ -0,0 +1,7 @@
+namespace NovaAccounts.Components.DataFields;
+
+public class DataDisplayItem
+{
+    public string Label { get; set; } = string.Empty;
+    public string Value { get; set; } = string.Empty;
+}
---
+++ NovaAccounts/Components/ModalFields/SelectOption.cs
@@ -0,0 +1,9 @@
+namespace NovaAccounts.Components.ModalFields;
+
+public class SelectOption
+{
+    public string Value { get; set; } = "";
+    public string Text { get; set; } = "";
+    public bool Selected { get; set; } = false;
+    public bool Disabled { get; set; } = false;
+}
