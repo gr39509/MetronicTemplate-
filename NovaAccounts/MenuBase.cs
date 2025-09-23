7d09d778104ccc16b8d8377eef47bdbc5cf93c26
Tahiru22 <ktahiru181@gmail.com>
2025-09-23 16:04:57 +0000

    charts

--- NovaAccounts/MenuBase.cs
+++ NovaAccounts/MenuBase.cs
@@ -2,9 +2,19 @@ namespace NovaAccounts;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Routing;
 
-public class MenuBase : ComponentBase
+public class MenuBase : ComponentBase, IDisposable
 {
     [Inject] protected NavigationManager NavigationManager { get; set; } = null!;
+
+    protected override void OnInitialized()
+    {
+        NavigationManager.LocationChanged += OnLocationChanged;
+    }
+
+    protected virtual void OnLocationChanged(object? sender, LocationChangedEventArgs e)
+    {
+        InvokeAsync(StateHasChanged);
+    }
 
     protected string GetCurrentController()
     {
@@ -13,7 +23,12 @@ public class MenuBase : ComponentBase
         return segments.Length > 0 ? segments[0] : "";
     }
 
-  
+    protected string GetCurrentAction()
+    {
+        var uri = new Uri(NavigationManager.Uri);
+        var segments = uri.LocalPath.Split('/', StringSplitOptions.RemoveEmptyEntries);
+        return segments.Length > 1 ? segments[1] : "";
+    }
 
     protected bool IsControllerActive(List<string> controllers)
     {
@@ -23,9 +38,28 @@ public class MenuBase : ComponentBase
 
     protected bool IsCurrentPage(string path)
     {
+        var currentUri = new Uri(NavigationManager.Uri);
+        var currentPath = currentUri.LocalPath.TrimEnd('/');
+        var comparePath = path.TrimEnd('/');
         
+        // Handle root path
+        if (string.IsNullOrEmpty(currentPath) && (comparePath == "/" || string.IsNullOrEmpty(comparePath)))
+            return true;
+            
+        return currentPath.Equals(comparePath, StringComparison.InvariantCultureIgnoreCase);
+    }
+
+    protected bool IsCurrentPage(string controller, string action)
+    {
+        var currentController = GetCurrentController();
+        var currentAction = GetCurrentAction();
         
-        var uri = new Uri(NavigationManager.Uri);
-        return uri.LocalPath.Equals(path, StringComparison.InvariantCultureIgnoreCase);;
+        return currentController.Equals(controller, StringComparison.InvariantCultureIgnoreCase) &&
+               currentAction.Equals(action, StringComparison.InvariantCultureIgnoreCase);
+    }
+
+    public void Dispose()
+    {
+        NavigationManager.LocationChanged -= OnLocationChanged;
     }
 }
