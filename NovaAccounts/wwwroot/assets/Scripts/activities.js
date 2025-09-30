a49f6bff526458f847fe62afc598ddbf1060adaa
Tahiru22 <ktahiru181@gmail.com>
2025-09-30 17:54:27 +0000

    Search box

---
+++ NovaAccounts/wwwroot/assets/Scripts/activities.js
@@ -0,0 +1,4 @@
+var header_notification_bar = $('#header_notification_bar');
+var notificate = $('#notificate');
+header_notification_bar.empty();
+header_notification_bar.load('/Home/_LoadNotifications/');
